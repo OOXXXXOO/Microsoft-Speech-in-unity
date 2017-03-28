c2e9aedba37fec90efd00685daa30b93e86290ef
Tom Winshare <785929784@qq.com>
2017-03-28 23:22:08 +0800

    first commit

---
+++ Assets/script/speechtest.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+using UnityEngine.UI;
+public class speechtest : MonoBehaviour {
+
+
+	//关键词识别器
+
+    //______________________________________________________________________________
+	KeywordRecognizer keywordRecognizer;
+	Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+	private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+	{
+		System.Action keywordAction;
+		// 如果识别到关键词就调用
+		if (keywords.TryGetValue(args.text, out keywordAction))
+		{
+			keywordAction.Invoke();
+
+			Debug.Log ("args text is"+args.text);
+		}
+	}
+	void Start () {
+
+		Debug.Log ("script _ processing");
+
+
+		//初始化指令集
+
+		keywords.Add("我想要一个红色的方块", () =>{
+				Debug.Log("red");
+			
+			});
+		keywords.Add("我想要一个蓝色的方块", () =>{
+				Debug.Log("bule");
+			
+			});
+		keywords.Add ("你好", () => {
+			Debug.Log ("你好");
+	     	});
+        /*
+         * 
+         keywords.Add ("keyword", () => {
+			//action
+	     	});
+         */
+
+
+
+
+
+
+		keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+		keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+		//开始识别
+		keywordRecognizer.Start();
+	
+	}
+
+}
---
+++ Assets/script/speechtest1.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+using UnityEngine.UI;
+public class speechtest1 : MonoBehaviour {
+    DictationRecognizer dictationRecognizer;
+
+    //听写功能正常但需要在x86下编译则表明所有的功能都需要在x86架构下编译生效才可以确定  该功能可用后续放弃是否依据结果判定
+    // Use this for initialization
+    void Start()
+    {
+        dictationRecognizer = new DictationRecognizer();
+
+        dictationRecognizer.DictationResult += onDictationResult;
+        dictationRecognizer.DictationHypothesis += onDictationHypothesis;
+        dictationRecognizer.DictationComplete += onDictationComplete;
+        dictationRecognizer.DictationError += onDictationError;
+
+        dictationRecognizer.Start();
+    }
+
+    void onDictationResult(string text, ConfidenceLevel confidence)
+    {
+        // write your logic here
+        Debug.LogFormat("Dictation result: " + text);
+        
+    }
+
+    void onDictationHypothesis(string text)
+    {
+        // write your logic here
+        Debug.LogFormat("Dictation hypothesis: {0}", text);
+    }
+
+    void onDictationComplete(DictationCompletionCause cause)
+    {
+        // write your logic here
+        if (cause != DictationCompletionCause.Complete)
+            Debug.LogErrorFormat("Dictation completed unsuccessfully: {0}.", cause);
+    }
+
+    void onDictationError(string error, int hresult)
+    {
+        // write your logic here
+        Debug.LogErrorFormat("Dictation error: {0}; HResult = {1}.", error, hresult);
+    }
+
+
+
+
+}
